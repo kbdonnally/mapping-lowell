06a68d430f6f1e7e29c7a5fd79f70f51219be625
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-03 16:49:17 -0500

    Clean up <head> in common/ dir

--- common/header.php
+++ common/header.php
@@ -2,26 +2,19 @@
 <html class="<?php echo get_theme_option('Style Sheet'); ?>" lang="<?php echo get_html_lang(); ?>">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes">
     <?php if ($description = option('description')): ?>
-    <meta name="description" content="<?php echo $description; ?>" />
+    <meta name="description" content="<?php echo $description; ?>">
     <?php endif; ?>
 
-    <?php
-    if (isset($title)) {
-        $titleParts[] = strip_formatting($title);
-    }
-    $titleParts[] = option('site_title');
-    ?>
-    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+    <title><?php echo option('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
 
     <?php echo auto_discovery_link_tags(); ?>
 
     <?php fire_plugin_hook('public_head',array('view'=>$this)); ?>
     <!-- Stylesheets -->
     <?php
-    queue_css_file(array('iconfonts', 'style'));
-
+    queue_css_file(array('style'));
     echo head_css();
     ?>
     <!-- JavaScripts -->
