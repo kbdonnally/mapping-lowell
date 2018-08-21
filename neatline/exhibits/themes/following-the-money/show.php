8e8c65d3fa8cb95a13c1316e66f614e7fda5f0a0
Katherine Donnally <kbd3ax@virginia.edu>
2018-08-21 15:09:21 -0400

    Use custom funcions to display narrative & exhibit markup

--- neatline/exhibits/themes/following-the-money/show.php
+++ neatline/exhibits/themes/following-the-money/show.php
@@ -10,39 +10,37 @@
 ?>
 
 <?php
-/* Functions: */
+// functions
 function import_file($filename, $filepath = 'exhibits/partials')
 {
 	return get_view()->partial($filepath . '/' . $filename . '.php');
 }
 
-/* Variables: */
+// variables
 $title 		 = nl_getExhibitField('title');
-$testPartial = import_file('test');
+$exhibit 	 = import_file('exhibit');
+$narrative 	 = import_file('narrative');
 ?>
 
-<!-- head & header: -->
+<!-- head & header -->
 <?php echo head(array(
   'title' => $title,
   'bodyclass' => 'neatline show'
 )); ?>
 
-<!-- page title: -->
+<!-- page title -->
 <h1><?php echo $title; ?></h1>
 
-<!-- file import test: -->
-<?php echo $testPartial; ?>
-
-<!-- fullscreen: -->
+<!-- fullscreen -->
 <?php echo nl_getExhibitLink(
   null, 'fullscreen', __('View Fullscreen'), array('class' => 'nl-fullscreen')
 ); ?>
 
-<!-- exhibit: -->
-<?php echo nl_getExhibitMarkup(); ?>
+<!-- exhibit -->
+<?php echo $exhibit; ?>
 
-<!-- narrative: -->
-<?php echo nl_getNarrativeMarkup(); ?>
+<!-- narrative -->
+<?php echo $narrative; ?>
 
 <!-- footer: -->
 <?php echo foot(); ?>
